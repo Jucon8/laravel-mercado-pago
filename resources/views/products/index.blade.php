aa52414695737f4e933a0017abdfdfd0f14fdd82
Juan José Condinanzi <jucon8@gmail.com>
2021-02-05 18:18:40 -0300

    laravel-mercado-pago

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,31 @@
+@extends('website')
+
+@section('content')
+<a href="{{url('carrito')}}"><button class="btn btn-danger">Carrito</button></a>
+    <div class="row">
+        @foreach($products as $product)
+            <div class="col-4">
+                <div class="card">
+                    <img src="{{ $product->picture_url }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $product->title }}</h5>
+                        <p class="card-text">{{ $product->description }}</p>
+                        <p class="card-text">{{ $product->currency_id }} {{ $product->unit_price }}</p>
+                    </div>
+                    <div class="card-footer">
+                        <form action="{{ url('/carrito', $product->id) }}" method="post">
+                            @csrf
+                            <button class="btn btn-primary">Comprar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <div class="row">
+        <div class="col">
+            {{ $products->links() }}
+        </div>
+    </div>
+@endsection
